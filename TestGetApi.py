aefb4669d2a84d7e86794dc5f7ff2567ff0e8d4d
jcorrido <jcorrido@stevens.edu>
2022-10-06 23:11:01 -0400

    added cicleci for the API

--- TestGetApi.py
+++ TestGetApi.py
@@ -10,12 +10,11 @@ class TestApi(unittest.TestCase):
         self.assertNotEqual(dict["hellogitworld"], None, "repository 'Complexity' should be in dictionary")
 
     def testCommitCountCorrect(self):
-        dict = getRepoCommits("mmandelb2201")
+        dict = getRepoCommits("richkempinski")
         self.assertNotEqual(len(dict), 0, "dictionary should not be empty")
         if len(dict) == 0:
             return
-        self.assertEqual(dict["portfolio"], 18, "repository 'portfolio' should have 18 commits")
-        self.assertEqual(dict["ssw-project-one"], 30, "repository 'ssw-project-one' should have 30 commits")
+        self.assertEqual(dict["hellogitworld"], 29, "repository 'hellogitworld' should have 29 commits")
 
 if __name__ == '__main__':
     print('Running unit tests')
